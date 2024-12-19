8fb7111cd9a1044183fb1b077135051c429ad045
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2024-12-19 15:52:38 -0500

    Updates

--- sales/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Sale
-
-# Register your models here.
-admin.site.register(Sale)
--- sales/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, records
-
-app_name = 'sales'
-
-urlpatterns = [
-    path('', home),
-    path('sales/', records),
-]
